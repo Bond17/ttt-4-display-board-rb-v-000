e3bd3de77ff74bad3225885ed68f631bd0989e45
Robert Watson <spartan83@gmail.com>
2017-12-02 05:04:06 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,6 @@
 # out the current state.
 def display_board (toe=Array.new)
 board =" #{toe[0]} | #{toe[1]} | #{toe[2]} \n-----------\n #{toe[3]} | #{toe[4]} | #{toe[5]}
-\n-----------\n #{toe[6]} | #{toe[7]} | #{toe[8]} "
+ \n-----------\n #{toe[6]} | #{toe[7]} | #{toe[8]} "
 puts board
 end
